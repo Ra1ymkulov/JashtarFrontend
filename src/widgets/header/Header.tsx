5945a0e2235e51abfaa49ef840c328041299575d
Ra1ymkulov <retype.uun@gmail.com>
2026-04-21 05:17:04 +0600

    added

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -4,8 +4,10 @@ import scss from "./Header.module.scss";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { IoCloseSharp } from "react-icons/io5";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
   const [menu, setMenu] = useState<boolean>(false);
   return (
     <header className={scss.header}>
@@ -28,7 +30,12 @@ const Header = () => {
             <div onClick={() => setMenu(true)} className={scss.burgerOpen}>
               <HiOutlineMenuAlt1 />
             </div>
-            <img className={scss.logo} src="/logo.svg" alt="" />
+            <img
+              onClick={() => router.push("/")}
+              className={scss.logo}
+              src="/logo.svg"
+              alt=""
+            />
             <nav>
               <a href="/">Главная</a>
               <a href="/about-movement">О движении</a>
